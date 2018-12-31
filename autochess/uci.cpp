2c79197c1e9ff0a96d0f3839dda8b98016217eea
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-12-30 23:06:09 -0800

    experimenting with std handles. not yet working

--- autochess/uci.cpp
+++ autochess/uci.cpp
@@ -9,10 +9,19 @@ UCIReader::UCIReader(string& path)
 void UCIReader::CreateProc()
 {
     cout << "Creating Process  " << path << endl;
+	OFSTRUCT of;
 	STARTUPINFO startInfo = { sizeof(STARTUPINFO) };
+	startInfo.lpTitle = (char*) "TITLE OF SCREEN";
+	startInfo.dwFlags = STARTF_USESTDHANDLES;
+	startInfo.hStdOutput = (HANDLE) OpenFile("mytextlolfile.txt", &of, OF_WRITE);
+	if (startInfo.hStdOutput == (HANDLE) HFILE_ERROR) {
+		cout << "FAIL" << endl;
+		cout << GetLastError() << endl;
+		cin.get();
+	}
 	PROCESS_INFORMATION procInfo;
-	LPSTR lpstr = (char*) "python C:\\Users\\peynu\\Desktop\\Project6.py";
-	if (CreateProcess(NULL, lpstr, NULL, NULL, FALSE, NULL, NULL, NULL, &startInfo, &procInfo))
+	LPSTR lpstr = (char*) "python lol.py";
+	if (CreateProcess(NULL, lpstr, NULL, NULL, FALSE, 0, NULL, NULL, &startInfo, &procInfo))
 		cout << "Success " << endl;
 	else
 		cout << "Failure " << GetLastError() << endl;
