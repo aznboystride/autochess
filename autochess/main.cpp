898e67f9d8baacb6be1d5e6fc366d095aedbde8d
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-12-31 12:28:25 -0800

    nothing much. realized that i cannot directly write to stdin

--- autochess/main.cpp
+++ autochess/main.cpp
@@ -1,15 +1,24 @@
-
-#include "move.h"
-#include "uci.h"
-#include "chesscom.h"
+#include <Windows.h>
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    string path = "path";
-    Uci uci(path);
-	
-	cout << "End of Main" << endl;
-	cin.get();
-    return 0;
+	HANDLE _stdin = GetStdHandle(STD_INPUT_HANDLE);
+	DWORD bytesWritten;
+	char answer[100] = "My name is anonymous";
+	string name;
+	cout << "What is your name: ";
+	if (WriteFile(_stdin, answer, strlen(answer), &bytesWritten, NULL)) {
+		cout << "strlen(answer) " << strlen(answer) << endl;
+		cout << "BytesWritten " << bytesWritten << endl;
+		cin >> name;
+		cout << "Hi " << name << endl;
+	}
+	else {
+		cout << "Error Code: " << GetLastError() << endl;
+	}
+	system("pause");
+	return 0;
 }
-
