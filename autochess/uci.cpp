e85c33aacf9680c8cad5bc40bb3faa153985384f
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2019-01-02 13:51:06 -0800

    implemented writetopipe and readfrompipe

--- autochess/uci.cpp
+++ autochess/uci.cpp
@@ -1,9 +1,35 @@
-#include "uci.h"
+#include "uci.h" 
 
-UCIReader::UCIReader(string& path)
-{
-    this->path = path;
-    CreateProc();
+UCIReader::UCIReader(string& path) 
+{ 
+    this->path = path; 
+  
+} 
+
+void UCIReader::CreateChildPipes() 
+{ 
+    SECURITY_ATTRIBUTES sAttr = { sizeof(SECURITY_ATTRIBUTES)  };
+    sAttr.lpSecurityDescriptor = NULL; 
+    sAttr.bInheritHandle = TRUE; 
+    BOOL bSuccess = FALSE; 
+    
+    bSuccess = CreatePipe( &hStdInRd, &hStdInWr, &sAttr, 0 ); 
+    if( !bSuccess  ) 
+        ErrorExit(TEXT("CreatePipe"));
+    
+    bSuccess = CreatePipe( &hStdOutRd, &hStdOutWr, &sAttr, 0 );
+    if( !bSuccess  )
+        ErrorExit(TEXT("CreatePipe"));
+
+    bSuccess = SetHandleInformation( &hStdInWr, HANDLE_FLAG_INHERIT, 0  );
+    if( !bSuccess )
+        ErrorExit(TEXT("SetHandleInformation"));
+    
+    bSuccess = SetHandleInformation( &hStdOutRd, HANDLE_FLAG_INHERIT, 0  );
+    if( !bSuccess  )
+        ErrorExit(TEXT("SetHandleInformation"));
+
+     
 }
 
 void UCIReader::CreateChildProcess(const TCHAR* applicationName)
@@ -15,9 +41,9 @@ void UCIReader::CreateChildProcess(const TCHAR* applicationName)
     ZeroMemory(&procInfo, sizeof(PROCESS_INFORMATION));
     ZeroMemory(&startupInfo, sizeof(STARTUPINFO));
     startupInfo.cb = sizeof(STARTUPINFO);
-    startupInfo.hStdError = hStdError;
-    startupInfo.hStdOutput = hStdOut;
-    startupInfo.hStdInput = hStdIn;
+    startupInfo.hStdError = hStdOutWr;
+    startupInfo.hStdOutput = hStdOutWr;
+    startupInfo.hStdInput = hStdInRd;
     startupInfo.dwFlags |= STARTF_USESTDHANDLES;
 
     bSuccess = CreateProcess(applicationName, NULL, NULL, TRUE, 0, NULL, NULL, &startupInfo, &procInfo);
@@ -28,6 +54,26 @@ void UCIReader::CreateChildProcess(const TCHAR* applicationName)
         CloseHandle(procInfo.hProcess);
         CloseHandle(procInfo.hThread);
     }
+}
+
+void UCIReader::WriteToPipe(CHAR* buff)
+{
+    BOOL bSuccess = FALSE;
+    DWORD bytesWritten;
+    bSuccess = WriteFile( hStdInWr, buff, strlen(buff), &bytesWritten, NULL );
+    if ( !bSuccess  )
+        ErrorExit( TEXT("WriteToPipe")  );
+}
+
+void UCIReader::ReadFromPipe()
+{
+    BOOL bSuccess = FALSE;
+    DWORD bytesWritten;
+    CHAR buff[4096];
+    bSuccess = ReadFile( hStdOutRd, buff, 4096, &bytesWritten, NULL );
+    if( !bSuccess )
+        ErrorExit( TEXT("ReadFromPipe") );
+    buff[bytesWritten] = 0;
 }
 
 string UCIReader::InsertCommand(string mv)
