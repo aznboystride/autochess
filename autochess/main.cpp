9ae8e3a512e804c17e59468ba7b36328db965027
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2019-01-03 09:07:12 -0800

    changed command from uciread to uci

--- autochess/main.cpp
+++ autochess/main.cpp
@@ -10,7 +10,7 @@ int main()
 	string applicationPath = "C:\\Users\\peynu\\Desktop\\stockfish-10-win\\Windows\\stockfish_10_x64.exe";
 	cout << "Attempting to create UCI object" << endl;
 	Uci* uci = new Uci(applicationName, applicationPath);
-	string command = "uciread";
+	string command = "uci";
 	uci->MakeMove(command);
 	system("pause");
 	return 0;
